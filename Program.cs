5d6f198b49f332953e81ef1abd216558216e0971
Ilia Mediankin <i.mediankin@gmail.com>
2023-02-13 23:15:36 +0300

    feat: program: add main

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿    // Написать программу, которая из имеющегося массива строк формирует 
+    // массив из строк, длина которых меньше либо равна 3 символа.
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        string[] inputArray1 = {"hello", "2", "world", ":-)"};
+        string[] inputArray2 = {"1234", "1567", "-2", "computer science"};
+        string[] inputArray3 = {"Russia", "Denmark", "Kazan"};
+    }
+}
